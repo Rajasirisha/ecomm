8c24f9fad7a40539b7693b5bb7db4ec581fc3a36
Rajasirisha <sirisha.siri11@gmail.com>
2024-04-08 20:54:21 +0530

    pages

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -7,8 +7,18 @@ const About = () => {
     <div>
         <Navbar />
         <Box height={40}/>
-        <Box component="main" >
-        <h1>About ecommerce</h1>
+        <Box component="main" sx={{ pl: 3}}>
+        <h1 style={{color: '#8D2F4F'}}>About Ecommerce</h1>
+        <p>One of the world's largest e-commerce platforms offers a vast array of products, 
+          ranging from electronics and clothing to groceries and entertainment. 
+          Known for its user-friendly interface and convenient delivery options, 
+          this platform has revolutionized online shopping. 
+          With a robust marketplace that includes both major brands and independent sellers, 
+          customers can find virtually anything they need with just a few clicks. Additionally, 
+          features like customer reviews and personalized recommendations enhance the shopping experience, 
+          making it easier for users to discover new products tailored to their preferences.
+           This e-commerce giant has become an indispensable part of many people's lives, 
+          providing unparalleled convenience and access to a wide selection of goods.</p>
         </Box>
         </div>
   )
